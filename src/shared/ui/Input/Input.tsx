972eeb1d8c3484034351fa89fb61558a000d25c1
AbdugafurovaUmida <abdugafurova93@gmail.com>
2024-05-07 16:19:57 +0500

    feat: reusable input component

---
+++ src/shared/ui/Input/Input.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+interface IInputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  type: string;
+  id: string;
+  placeholder?: string;
+  label?: string;
+  error?: string;
+}
+
+const Input = React.forwardRef<HTMLInputElement, IInputProps>(
+  ({ type, id, placeholder, label, error, ...restProps }, ref) => {
+    return (
+      <div>
+        {label && <label htmlFor={id}>{label}</label>}
+        <input
+          type={type}
+          id={id}
+          placeholder={placeholder}
+          className={`${'formInput'} ${error ? 'errorInput' : ''}`}
+          ref={ref}
+          {...restProps}
+        />
+        {error && <p className='error'>{error}</p>}
+      </div>
+    );
+  }
+);
+
+export default Input;
