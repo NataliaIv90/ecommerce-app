7f3cf2d3da4073f5c8ed75841aaaca9ebebf9bb9
Natalia Ivantsova <ivantsovana@gmail.com>
2024-05-09 12:01:34 +0300

    feature: add button for redirection to the registration page

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import { MainWrapper } from '../mainWrapper/MainWrapper';
 import { LoginForm } from './loginForm/LoginForm';
+import { Link } from 'react-router-dom';
+import { OutlinedButton } from '../../shared/button/outlinedButton/OutlinedButton';
 
 export const Login = (): JSX.Element => (
   <MainWrapper>
     <LoginForm />
+    <div>
+      <p className='loginFooter'>Don't have an account yet?</p>
+      <Link to='/registration'>
+        <OutlinedButton
+          text='Sign up'
+          wideBtn={true}
+        />
+      </Link>
+    </div>
   </MainWrapper>
 );
