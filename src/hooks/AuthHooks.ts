70b181d98232a788d478049f5e5bf21cd52799cb
AbdugafurovaUmida <abdugafurova93@gmail.com>
2024-05-09 17:40:06 +0500

    feat: add authhooks

---
+++ src/hooks/AuthHooks.ts
@@ -0,0 +1,26 @@
+import { useEffect, useState } from 'react';
+import { getApiRoot } from '../api/lib/Client';
+import { API } from '../api/API';
+import { useAppDispatch } from '../hooks/reduxHooks';
+
+export const useAuth = () => {
+  const [token, setToken] = useState('');
+  const [auth, setAuth] = useState(false);
+  const [apiClient, setApiClient] = useState(getApiRoot('anonimous'));
+  const [apiInstance, setApiInstance] = useState(new API(apiClient));
+  const dispatch = useAppDispatch();
+
+  const changeAuth = (flag: boolean): void => {
+    setAuth(flag);
+  };
+
+  useEffect(() => {
+    if (localStorage.getItem('tokendata')) {
+      const tokenLS = localStorage.getItem('tokendata') as string;
+      setToken(tokenLS || ('' as string));
+      setApiClient(getApiRoot('token', { token: token }));
+      setApiInstance(new API(apiClient));
+    }
+  }, [auth]);
+  return [changeAuth];
+};
