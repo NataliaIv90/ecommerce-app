2341e59394c323be9ef960d5015c589577aa8aa2
Natalia Ivantsova <102389022+NataliaIv90@users.noreply.github.com>
2024-05-15 11:55:32 +0300

    Feature/rss ecomm 2 15 (#23)
    * init: start 2_15 feature
    
    * feat: integrate api with form - different billing and shipping address
    
    * feat: integrate api with form - different billing and shipping address
    
    * fix: fix styles
    
    * refactor: delete commented lines

--- jest.config.js
+++ jest.config.js
@@ -5,7 +5,8 @@ module.exports = {
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   moduleNameMapper: {
     '\\.(css|less)$': 'identity-obj-proxy',
-    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/src/tests/mocks/fileMock.ts',
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/src/tests/mocks/fileMock.ts',
   },
   transform: {
     '^.+\\.(ts|tsx)$': 'ts-jest',
--- src/components/registration/registrationForm/registrationForm.tsx
+++ src/components/registration/registrationForm/registrationForm.tsx
@@ -1,11 +1,11 @@
 import Input from '../../../shared/ui/Input/Input';
 import * as Yup from 'yup';
-import { ECountrieOptions, IRegisterData } from '../../../types/types';
+import { ECountrieOptions, IRegisterData, IRegistrationRequestData } from '../../../types/types';
 import { useForm, Controller, SubmitHandler, Resolver } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { useAppDispatch } from '../../../hooks/reduxHooks';
 import { createNewCustomer } from '../../../store/slices/customerSlice';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './RegistrationForm.css';
 import { FormFooter } from '../formFooter/FormFooter';
 import { OutlinedButton } from '../../../shared/button/outlinedButton/OutlinedButton';
@@ -15,6 +15,30 @@ const countriesCodes = {
   Uzbekistan: 'UZ',
   Ukraine: 'UA',
 };
+
+const addressSchema = Yup.object().shape({
+  street: Yup.string().required('Street is a required field').min(1, 'Street should be min 1 character'),
+  city: Yup.string()
+    .required('City is required field')
+    .min(1, 'City should be min 1 characters')
+    .matches(/^([A-Z][a-z]*)$/g, 'City can only contain latin letters, start from capital letter'),
+  country: Yup.string()
+    .required('Country is a required field')
+    .oneOf([ECountrieOptions.ge, ECountrieOptions.ua, ECountrieOptions.uz]),
+  postalCode: Yup.string()
+    .required('Postal code is required')
+    .when(['country'], (country: ECountrieOptions[], schema) => {
+      if (country[0] === ECountrieOptions.ua) {
+        return schema.matches(/^\d{5}$/, 'Invalid postal code format for Ukraine');
+      } else if (country[0] === ECountrieOptions.uz) {
+        return schema.matches(/^\d{6}$/, 'Invalid postal code format for Uzbekistan');
+      } else if (country[0] === ECountrieOptions.ge) {
+        return schema.matches(/^\d{4}$/, 'Invalid postal code format for Georgia');
+      } else {
+        return schema;
+      }
+    }),
+});
 
 const validationSchema = Yup.object().shape({
   email: Yup.string()
@@ -51,28 +75,12 @@ const validationSchema = Yup.object().shape({
   dateOfBirth: Yup.date()
     .min(new Date('1900-01-01'), 'Date must be after 1900')
     .max(new Date(new Date().getFullYear() - 13, 11, 31), 'User should be at least 13 years'),
-  street: Yup.string().required('Street is a required field').min(1, 'Street should be min 1 character'),
-  city: Yup.string()
-    .required('City is required field')
-    .min(1, 'City should be min 1 characters')
-    .matches(/^([A-Z][a-z]*)$/g, 'City can only contain latin letters, start from capital letter'),
-  country: Yup.string()
-    .required('Country is a required field')
-    .oneOf([ECountrieOptions.ge, ECountrieOptions.ua, ECountrieOptions.uz]),
-  postalCode: Yup.string()
-    .required('Postal code is required')
-    .when(['country'], (country: ECountrieOptions[], schema) => {
-      if (country[0] === ECountrieOptions.ua) {
-        return schema.matches(/^\d{5}$/, 'Invalid postal code format for Ukraine');
-      } else if (country[0] === ECountrieOptions.uz) {
-        return schema.matches(/^\d{6}$/, 'Invalid postal code format for Uzbekistan');
-      } else if (country[0] === ECountrieOptions.ge) {
-        return schema.matches(/^\d{4}$/, 'Invalid postal code format for Georgia');
-      } else {
-        return schema;
-      }
-    }),
+
+  shippingAddress: addressSchema,
+  billingAddress: addressSchema,
   defaultShippingAddress: Yup.boolean(),
+  defaultBillingAddress: Yup.boolean(),
+  billingAddressSameAsShipping: Yup.boolean(),
 });
 
 export const RegistrationForm = (): JSX.Element => {
@@ -83,30 +91,48 @@ export const RegistrationForm = (): JSX.Element => {
   const [requestError, setRequestError] = useState(false);
 
   const onSubmit: SubmitHandler<IRegisterData> = (data: IRegisterData) => {
-    if (data.email && data.password && data.firstName && data.dateOfBirth && data.country) {
+    if (data.email && data.password && data.firstName && data.dateOfBirth && data.shippingAddress.country) {
       const year = data.dateOfBirth.getFullYear();
       const month = String(data.dateOfBirth.getMonth() + 1).padStart(2, '0');
       const day = String(data.dateOfBirth.getDate()).padStart(2, '0');
       const formattedDate = `${year}-${month}-${day}`;
-      const requestData = {
+
+      const shippingAddress = {
+        firstName: data.firstName.trim(),
+        lastName: data.lastName.trim(),
+        streetName: data.shippingAddress.street.trim(),
+        postalCode: data.shippingAddress.postalCode,
+        city: data.shippingAddress.city,
+        country: countriesCodes[data.shippingAddress.country],
+      };
+
+      let billingAddress;
+      if (data.billingAddressSameAsShipping) {
+        billingAddress = shippingAddress;
+      } else {
+        billingAddress = {
+          firstName: data.firstName.trim(),
+          lastName: data.lastName.trim(),
+          streetName: data.billingAddress.street.trim(),
+          postalCode: data.billingAddress.postalCode,
+          city: data.billingAddress.city,
+          country: countriesCodes[data.billingAddress.country],
+        };
+      }
+
+      const requestData: IRegistrationRequestData = {
         email: data.email,
-        firstName: data.firstName,
-        lastName: data.lastName,
+        firstName: data.firstName.trim(),
+        lastName: data.lastName.trim(),
         password: data.password,
         dateOfBirth: formattedDate,
-        addresses: [
-          {
-            firstName: data.firstName.trim(),
-            lastName: data.lastName.trim(),
-            streetName: data.street.trim(),
-            postalCode: data.postalCode,
-            city: data.city,
-            country: countriesCodes[data.country],
-          },
-        ],
+        addresses: [shippingAddress, billingAddress],
+        shippingAddresses: [0],
+        billingAddresses: [1],
         defaultShippingAddress: data.defaultShippingAddress ? 0 : undefined,
-        defaultBillingAddress: data.defaultShippingAddress ? 0 : undefined,
+        defaultBillingAddress: data.defaultBillingAddress ? 1 : undefined,
       };
+
       void dispatch(createNewCustomer({ ...requestData, setOpen })).then((response) => {
         if (createNewCustomer.fulfilled.match(response)) {
           const customerData = response.payload.data;
@@ -121,23 +147,52 @@ export const RegistrationForm = (): JSX.Element => {
     }
   };
 
-  const { control, handleSubmit } = useForm<IRegisterData>({
+  const { control, handleSubmit, watch, setValue } = useForm<IRegisterData>({
     defaultValues: {
       email: '',
       firstName: '',
       lastName: '',
-      street: '',
       password: '',
       repeatPassword: '',
       dateOfBirth: new Date('1900-01-01'),
-      country: ECountrieOptions.ge,
-      city: '',
-      postalCode: '',
       defaultShippingAddress: false,
+      defaultBillingAddress: false,
+      billingAddressSameAsShipping: false,
+      shippingAddress: {
+        city: '',
+        country: ECountrieOptions.ge,
+        postalCode: '',
+        street: '',
+      },
+      billingAddress: {
+        city: '',
+        country: ECountrieOptions.ge,
+        postalCode: '',
+        street: '',
+      },
     },
     resolver: yupResolver(validationSchema) as Resolver<IRegisterData>,
     mode: 'all',
   });
+
+  const shippingAddress = watch('shippingAddress');
+  const billingAddressSameAsShipping = watch('billingAddressSameAsShipping');
+
+  useEffect(() => {
+    if (billingAddressSameAsShipping) {
+      setValue('billingAddress.country', shippingAddress.country);
+      setValue('billingAddress.street', shippingAddress.street);
+      setValue('billingAddress.city', shippingAddress.city);
+      setValue('billingAddress.postalCode', shippingAddress.postalCode);
+    }
+  }, [
+    billingAddressSameAsShipping,
+    setValue,
+    shippingAddress.city,
+    shippingAddress.country,
+    shippingAddress.postalCode,
+    shippingAddress.street,
+  ]);
 
   return (
     <div>
@@ -245,17 +300,19 @@ export const RegistrationForm = (): JSX.Element => {
           )}
         />
 
+        <h3 className='form-subtitle'>Shipping address information</h3>
         <div className='divider'></div>
 
-        <p>Shipping / billing address information</p>
         <Controller
           control={control}
-          name='country'
+          name='shippingAddress.country'
           render={({ field, fieldState }) => (
             <>
               <select
                 {...field}
+                name='shippingAddressCountry'
                 className='registration__select-input'
+                title='Shipping country'
               >
                 {Object.entries(ECountrieOptions).map(([key, value]) => (
                   <option
@@ -274,7 +331,7 @@ export const RegistrationForm = (): JSX.Element => {
 
         <Controller
           control={control}
-          name='street'
+          name='shippingAddress.street'
           render={({ field, fieldState }) => (
             <Input
               type='text'
@@ -290,7 +347,7 @@ export const RegistrationForm = (): JSX.Element => {
 
         <Controller
           control={control}
-          name='city'
+          name='shippingAddress.city'
           render={({ field, fieldState }) => (
             <Input
               type='text'
@@ -306,7 +363,7 @@ export const RegistrationForm = (): JSX.Element => {
 
         <Controller
           control={control}
-          name='postalCode'
+          name='shippingAddress.postalCode'
           render={({ field, fieldState }) => (
             <Input
               type='text'
@@ -334,7 +391,134 @@ export const RegistrationForm = (): JSX.Element => {
                   className={fieldState.error ? 'errorInput' : ''}
                 />
 
-                <label htmlFor='defaultShippingAddress'>Set as default address</label>
+                <label htmlFor='defaultShippingAddress'>Set as default shipping address</label>
+              </div>
+              {fieldState.error && <p className={fieldState.error ? 'error' : ''}>{fieldState.error.message}</p>}
+            </>
+          )}
+        />
+
+        <Controller
+          control={control}
+          name='billingAddressSameAsShipping'
+          render={({ field, fieldState }) => (
+            <>
+              <div className='registration__checkbox-wrapper'>
+                <input
+                  type='checkbox'
+                  id='billingAddressSameAsShipping'
+                  checked={field.value}
+                  onChange={field.onChange}
+                  className={fieldState.error ? 'errorInput' : ''}
+                />
+
+                <label htmlFor='billingAddressSameAsShipping'>Set the same address for billing</label>
+              </div>
+              {fieldState.error && <p className={fieldState.error ? 'error' : ''}>{fieldState.error.message}</p>}
+            </>
+          )}
+        />
+
+        <h3 className='form-subtitle'>Billing address information</h3>
+        {billingAddressSameAsShipping ? (
+          <p className='error'>*Disabled for input. Shipping and billing adresses are equal.</p>
+        ) : null}
+
+        <div className='divider'></div>
+
+        <Controller
+          control={control}
+          name='billingAddress.country'
+          render={({ field, fieldState }) => (
+            <>
+              <select
+                {...field}
+                name='billingAddressCountry'
+                className='registration__select-input'
+                disabled={billingAddressSameAsShipping}
+                title='Billing address country'
+              >
+                {Object.entries(ECountrieOptions).map(([key, value]) => (
+                  <option
+                    key={key}
+                    value={value}
+                  >
+                    {value}
+                  </option>
+                ))}
+              </select>
+
+              {fieldState.error && <p className={fieldState.error ? 'error' : ''}>{fieldState.error.message}</p>}
+            </>
+          )}
+        />
+
+        <Controller
+          control={control}
+          name='billingAddress.street'
+          render={({ field, fieldState }) => (
+            <Input
+              type='text'
+              id='streetBilling'
+              placeholder='Enter your street'
+              value={field.value}
+              onChange={field.onChange}
+              onBlur={field.onBlur}
+              error={fieldState.error?.message}
+              disabled={billingAddressSameAsShipping}
+            />
+          )}
+        />
+
+        <Controller
+          control={control}
+          name='billingAddress.city'
+          render={({ field, fieldState }) => (
+            <Input
+              type='text'
+              id='cityBilling'
+              placeholder='Enter your city'
+              value={field.value}
+              onChange={field.onChange}
+              onBlur={field.onBlur}
+              error={fieldState.error?.message}
+              disabled={billingAddressSameAsShipping}
+            />
+          )}
+        />
+
+        <Controller
+          control={control}
+          name='billingAddress.postalCode'
+          render={({ field, fieldState }) => (
+            <Input
+              type='text'
+              id='postalCodeBilling'
+              placeholder='Enter your postal code'
+              value={field.value}
+              onChange={field.onChange}
+              onBlur={field.onBlur}
+              error={fieldState.error?.message}
+              disabled={billingAddressSameAsShipping}
+            />
+          )}
+        />
+
+        <Controller
+          control={control}
+          name='defaultBillingAddress'
+          render={({ field, fieldState }) => (
+            <>
+              <div className='registration__checkbox-wrapper'>
+                <input
+                  type='checkbox'
+                  id='defaultBillingAddress'
+                  checked={field.value}
+                  onChange={field.onChange}
+                  className={fieldState.error ? 'errorInput' : ''}
+                />
+
+                <label htmlFor='defaultBillingAddress'>Set as default billing address</label>
               </div>
               {fieldState.error && <p className={fieldState.error ? 'error' : ''}>{fieldState.error.message}</p>}
             </>
