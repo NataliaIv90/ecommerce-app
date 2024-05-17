cec0c609dc259f4b966d8397ab81e45ff09567b7
Natalia Ivantsova <ivantsovana@gmail.com>
2024-05-17 15:08:00 +0300

    feat: add greeting section

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -4,6 +4,26 @@ import { MainWrapper } from '../mainWrapper/MainWrapper';
 import './Main.css';
 import { OutlinedButton } from '../../shared/button/outlinedButton/OutlinedButton';
 // import { Link } from 'react-router-dom';
+import { CarIcon, PhotoIcon, PaymentIcon, ChatIcon } from './mainCards/MainCards';
+
+const cardsData = [
+  {
+    icon: <PaymentIcon />,
+    text: 'Easy payment',
+  },
+  {
+    icon: <CarIcon />,
+    text: 'Delivery on time',
+  },
+  {
+    icon: <PhotoIcon />,
+    text: 'Real photo',
+  },
+  {
+    icon: <ChatIcon />,
+    text: 'Help with the selection',
+  },
+];
 
 export const Main: React.FC = () => {
   // Todo: uncomment lines related to navigation after 'catalog' page available
@@ -25,15 +45,35 @@ export const Main: React.FC = () => {
             {/* </Link> */}
           </div>
         </section>
+
         <section className='greeting'>
-          <p>The House with flowers welcomes you!</p>
-          <p>
-            We create beautiful decor, make beautiful bouquets for the holidays.Our friendly and creative team will be
-            happy to work
-          </p>
-          <p>with you! You can learn more about our company by clicking on the link</p>
+          <div className='greeting__content'>
+            <>
+              <div className='greeting__text'>
+                <p>The House with flowers welcomes you!</p>
+                <p>
+                  We create beautiful decor, make beautiful bouquets for the holidays.Our friendly and creative team
+                  will be happy to work with you!
+                </p>
+                <p>You can learn more about our company by clicking on the link</p>
+              </div>
+              <OutlinedButton text='learn more' />
+            </>
+          </div>
+          <div className='gretting__image'></div>
         </section>
-        <section className='why-us'></section>
+
+        <section className='why-us'>
+          <h2>Why us?</h2>
+          <div className='icons-container'>
+            {cardsData.map((el) => (
+              <div className='main__card'>
+                <div className='main__card-icon'>{el.icon}</div>
+                <p className='main__card-title'>{el.text}</p>
+              </div>
+            ))}
+          </div>
+        </section>
       </>
     </MainWrapper>
   );
