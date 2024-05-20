c878e1d0f46bae69d3490b36819ae9829de73786
MariamKochadze <kochadzemariam80@gmail.com>
2024-05-20 16:58:28 +0400

    fix:delete console logs

--- src/api/API.ts
+++ src/api/API.ts
@@ -51,10 +51,8 @@ export class API {
 
   async signIn(credentials: Credentials): Promise<CustomerSignInResult> {
     const result: CustomerSignInResult = {} as CustomerSignInResult;
-    console.log('signin cred', credentials);
     try {
       const result = await this.client.me().login().post({ body: credentials }).execute();
-      console.log('login success:', result);
       return result.body;
       // eslint-disable-next-line
     } catch (error) {}
@@ -63,10 +61,8 @@ export class API {
 
   async signInByToken(): Promise<Customer> {
     const result: Customer = {} as Customer;
-    console.log('signIn token');
     try {
       const result = await this.client.me().get().execute();
-      console.log('login success:', result);
       return result.body;
       // eslint-disable-next-line
     } catch (error) {}
