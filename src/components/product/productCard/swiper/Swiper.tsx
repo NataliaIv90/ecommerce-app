a6f1683977bacdc8a5a469e8f65e51eafc6cf4bc
Natalia Ivantsova <ivantsovana@gmail.com>
2024-05-30 14:13:40 +0300

    refactor: refactor the code

--- src/components/product/productCard/swiper/Swiper.tsx
+++ src/components/product/productCard/swiper/Swiper.tsx
@@ -12,33 +12,39 @@ interface IImageCarousel {
   currentIndex: number;
 }
 
+const renderSwiperSlides = (images: { url: string }[], currentIndex: number) => {
+  return images.map((image, index) => (
+    <SwiperSlide key={index}>
+      <div className='image-container'>
+        <img
+          src={image.url}
+          alt={`Slide ${index + 1}`}
+          className='carousel-image'
+          slot={index === currentIndex ? 'container-start' : undefined}
+        />
+      </div>
+    </SwiperSlide>
+  ));
+};
+
 export const ImageCarousel: FC<IImageCarousel> = ({ images, currentIndex }) => {
   return (
-    <div className='image-carousel'>
-      <Swiper
-        modules={[Navigation, Pagination, Scrollbar, A11y]}
-        slidesPerView={1}
-        spaceBetween={30}
-        loop={true}
-        pagination={{
-          clickable: true,
-        }}
-        initialSlide={currentIndex}
-        navigation={true}
-      >
-        {images.map((image, index) => (
-          <SwiperSlide key={index}>
-            <div className='image-container'>
-              <img
-                src={image.url}
-                alt={`Slide ${index + 1}`}
-                className='carousel-image'
-                slot={index === currentIndex ? 'container-start' : undefined}
-              />
-            </div>
-          </SwiperSlide>
-        ))}
-      </Swiper>
+    <div className='carousel-wrapper'>
+      <div className='image-carousel'>
+        <Swiper
+          modules={[Navigation, Pagination, Scrollbar, A11y]}
+          slidesPerView={1}
+          spaceBetween={30}
+          loop={true}
+          pagination={{
+            clickable: true,
+          }}
+          initialSlide={currentIndex}
+          navigation={true}
+        >
+          {renderSwiperSlides(images, currentIndex)}
+        </Swiper>
+      </div>
     </div>
   );
 };
