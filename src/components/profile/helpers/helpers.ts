800ae1e85496db6a9f56232c3fd89ab7bc9f2030
MariamKochadze <kochadzemariam80@gmail.com>
2024-06-01 17:46:08 +0400

    feat: add editting mode functionality

--- src/components/profile/helpers/helpers.ts
+++ src/components/profile/helpers/helpers.ts
@@ -1,37 +1,33 @@
 import { Address, Customer } from '@commercetools/platform-sdk';
 
-export const getShippingAddresses = (customer: Customer | null): Address[] | null => {
-  return (
-    customer &&
-    customer?.addresses.filter((address: Address) => {
-      let temp;
-      customer.shippingAddressIds?.forEach((id) => {
-        if (id === address.id) {
-          temp = address;
-          return;
-        }
-      });
+export const getShippingAddresses = (customer: Customer | null): Address[] | [] => {
+  if (!customer) return [];
+  return customer.addresses.filter((address: Address) => {
+    let temp;
+    customer.shippingAddressIds?.forEach((id) => {
+      if (id === address.id) {
+        temp = address;
+        return;
+      }
+    });
 
-      return temp;
-    })
-  );
+    return temp;
+  });
 };
 
-export const getBillinggAddresses = (customer: Customer | null): Address[] | null => {
-  return (
-    customer &&
-    customer?.addresses.filter((address: Address) => {
-      let temp;
-      customer.shippingAddressIds?.forEach((id) => {
-        if (id === address.id) {
-          temp = address;
-          return;
-        }
-      });
+export const getBillinggAddresses = (customer: Customer | null): Address[] | [] => {
+  if (!customer) return [];
+  return customer.addresses.filter((address: Address) => {
+    let temp;
+    customer.shippingAddressIds?.forEach((id) => {
+      if (id === address.id) {
+        temp = address;
+        return;
+      }
+    });
 
-      return temp;
-    })
-  );
+    return temp;
+  });
 };
 
 export const isDefault = (address: Address, defaultAddressId: string | undefined): boolean =>
