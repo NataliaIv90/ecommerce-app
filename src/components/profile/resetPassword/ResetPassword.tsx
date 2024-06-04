487a807b9310a6ffce47d5c3e0c8d52c0ea850eb
MariamKochadze <kochadzemariam80@gmail.com>
2024-06-04 13:52:28 +0400

    feat: add password change functionality

--- src/components/profile/resetPassword/ResetPassword.tsx
+++ src/components/profile/resetPassword/ResetPassword.tsx
@@ -3,22 +3,46 @@ import { Controller, Resolver, useForm } from 'react-hook-form';
 import Input from '../../../shared/ui/Input/Input';
 import { OutlinedButton } from '../../../shared/button/outlinedButton/OutlinedButton';
 import validationSchema from './validationSchema';
+import { useAppDispatch } from '../../../hooks/reduxHooks';
+import { ChangePassword } from '../../../store/slices/customerSlice';
+import { setLoading } from '../../../store/slices/productSlice';
 
-interface IResetPassword {
-  password: string;
+interface IChnagePassword {
+  currentPassword: string;
+  newPassword: string;
 }
 
 const ResetPassword = ({ handleEditToggle }: { handleEditToggle: () => void }): JSX.Element => {
-  const { control, handleSubmit } = useForm<IResetPassword>({
+  const dispatch = useAppDispatch();
+
+  const { control, handleSubmit } = useForm<IChnagePassword>({
     defaultValues: {
-      password: '',
+      currentPassword: '',
+      newPassword: '',
     },
-    resolver: yupResolver(validationSchema) as Resolver<IResetPassword>,
+    resolver: yupResolver(validationSchema) as Resolver<IChnagePassword>,
     mode: 'all',
   });
 
-  const onSubmit = (data: IResetPassword) => {
-    return 5;
+  const onSubmit = (data: IChnagePassword) => {
+    if (data.currentPassword.trim() && data.newPassword.trim()) {
+      const chnagePasswordFields: IChnagePassword = {
+        currentPassword: data.currentPassword.trim(),
+        newPassword: data.newPassword.trim(),
+      };
+
+      setLoading(true);
+      void dispatch(ChangePassword(chnagePasswordFields)).then((response) => {
+        if (ChangePassword.fulfilled.match(response)) {
+          const customerData = response.payload;
+          if (customerData) {
+            alert('Password Successfully Chnaged!');
+          }
+          handleEditToggle();
+          setLoading(false);
+        }
+      });
+    }
   };
 
   return (
@@ -26,15 +50,36 @@ const ResetPassword = ({ handleEditToggle }: { handleEditToggle: () => void }): 
       className='profile-form'
       onSubmit={handleSubmit(onSubmit)}
     >
+      <label htmlFor='currentPassword'>Current Password: </label>
       <Controller
         control={control}
-        name='password'
+        name='currentPassword'
         render={({ field, fieldState }) => (
           <Input
+            name='currentPassword'
             type='password'
-            id='password'
+            id='currentPassword'
+            placeholder='Enter current password'
+            autoComplete='currentPassword'
+            value={field.value}
+            onChange={field.onChange}
+            onBlur={field.onBlur}
+            error={fieldState.error?.message}
+          />
+        )}
+      />
+
+      <label htmlFor='newPassword'>New Password: </label>
+      <Controller
+        control={control}
+        name='newPassword'
+        render={({ field, fieldState }) => (
+          <Input
+            name='newPassword'
+            type='password'
+            id='newPassword'
             placeholder='Enter new password'
-            autoComplete='password'
+            autoComplete='newPassword'
             value={field.value}
             onChange={field.onChange}
             onBlur={field.onBlur}
